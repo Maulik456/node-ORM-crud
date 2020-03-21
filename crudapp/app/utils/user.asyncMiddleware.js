9b404798f8394155d674d5b2c44cd0db7b007a56
Maulik456 <prajapatimaulik21@gmail.com>
2020-03-21 17:13:20 +0530

    Remove unnecessary JS and templates

--- crudapp/app/utils/user.asyncMiddleware.js
+++ crudapp/app/utils/user.asyncMiddleware.js
@@ -1,6 +1,3 @@
-/*const asyncMiddleware = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};*/
 /*module.exports = (fn) => {
 	return (req, res, next) => {
 		Promise.resolve(fn(req, res, next)).catch(next);
